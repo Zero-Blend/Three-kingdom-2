f9d2adc80c499ccfa4edd0116e3c88203a6c9ca0
SugarFree <kingckh9810@gmail.com>
2026-04-09 18:58:48 +0800

    Update generals-data.js

--- generals-data.js
+++ generals-data.js
@@ -8,7 +8,7 @@ const generals = [
     aptitude: "15",
     prefix: "天命",
     troop: "戎車",
-    image: "caocao.jpg",
+    image: "Tianmingcaocao.jpg",
     bio: "魏天命核心輸出。",
     skills: {
       lord: {
